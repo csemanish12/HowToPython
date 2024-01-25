9ad0045ca418940fb4075b3649a95292fef21948
Manish Sah <sahmanish20@gmail.com>
2024-01-25 19:52:09 +0545

    add sample for builtin namespace

---
+++ basics/namespaces/builtin_namespace.py
@@ -0,0 +1,4 @@
+elements = [1, 2, 4, 5, 10, 6, 21, 12, 0]
+max_element = max(elements)   # max is built-in python method which resides in built-in namespace.
+print("max element is:", max_element)
+
